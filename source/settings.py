c4406071332a82ca337c5843d0f253300c7b1be8
MaisimZaman <81390092+MaisimZaman@users.noreply.github.com>
2021-05-01 20:24:49 -0400

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os 
+import mimetypes
+
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
