54a5aa069d8ed6a7144f7f95e307a2d8047351c3
MaisimZaman <81390092+MaisimZaman@users.noreply.github.com>
2021-05-01 20:42:31 -0400

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'media_cdn'),
 ]
 
-STATIC_URL = '/frontend/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
